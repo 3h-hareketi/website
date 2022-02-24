d9fa00c6959c9da5cfee1bf22e40830593df9e41
Ant Somers <antsomers@gmail.com>
2022-02-24 03:03:10 +0300

    Revert "Integrate flowbite"
    This reverts commit 2f4082b66d9947217f4904d8b293da2444329389.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   content: [
-    "./node_modules/flowbite/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
