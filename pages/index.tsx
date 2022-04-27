dd679761182f5445cfc0453f1f4a462464cf468d
Yanki <yankisirtas@hotmail.com>
2022-04-27 17:54:19 +0300

    Add hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,23 @@ import Newsletter from "../components/Newsletter";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex h-screen ">
-      {/* <FeaturedBlogs /> */}
-      {/* <div className="m-auto">
-          <Newsletter />
-        </div> */}
+    <div className="flex h-screen mx-auto">
+      <div className="mx-auto mt-16 text-white">
+        {" "}
+        <h1 className="font-extrabold text-8xl">
+          Discover liberalism, <br /> the ideology of freedom!
+        </h1>
+        <div className="max-w-2xl mt-10">
+          Vulputate erat cubilia praesent, dolor mattis condimentum eget tellus
+          phasellus auctor elementum morbi, justo torquent egestas, nulla cursus
+          tellus, per.
+        </div>
+        <button className="p-2 mt-4 font-medium text-black bg-white shadow-md w-36 rounded-2xl hover:text-primary-500 hover:shadow-lg">
+          SEE MORE
+        </button>
+      </div>
+      {/* <FeaturedBlogs />
+      <Newsletter /> */}
     </div>
   );
 };
