52f0be2063d6ead33a4002da0d1725d864cdf28c
Yanki <yankisirtas@hotmail.com>
2022-04-27 17:22:36 +0300

    Remove duplicate layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,12 @@ import Newsletter from "../components/Newsletter";
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <div className="flex h-screen bg-gradient-radial from-primary-800 to-primary-400">
-        {/* <FeaturedBlogs /> */}
-        {/* <div className="m-auto">
+    <div className="flex h-screen bg-gradient-radial from-primary-800 to-primary-400">
+      {/* <FeaturedBlogs /> */}
+      {/* <div className="m-auto">
           <Newsletter />
         </div> */}
-      </div>
-    </Layout>
+    </div>
   );
 };
 
