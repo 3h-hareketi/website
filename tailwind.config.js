15be4213d50bb3186fb0d1f4051f1a5e5cda8dee
Yanki <yankisirtas@hotmail.com>
2022-04-27 13:35:10 +0300

    Add primary theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   ],
   theme: {
     colors: {
-      logoColor: "#12acbc",
+      primary: {
+        50: "#f3fbfc",
+        100: "#e7f7f8",
+        200: "#c4eaee",
+        300: "#a0dee4",
+        400: "#59c5d0",
+        500: "#12acbc",
+        600: "#109ba9",
+        700: "#0e818d",
+        800: "#0b6771",
+        900: "#09545c",
+      },
     },
   },
   plugins: [],
