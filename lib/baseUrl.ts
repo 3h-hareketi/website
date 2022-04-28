2aaefca605184cbc73340f4b8364a5d6843786cf
Yanki <yankisirtas@hotmail.com>
2022-04-28 16:18:47 +0300

    Fix #37

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,5 +1,5 @@
 const BASE_URL =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "production" || process.env.VERCEL_ENV === "preview"
     ? "https://" + process.env.VERCEL_URL
     : "http://localhost:3000";
 
