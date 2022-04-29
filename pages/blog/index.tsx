c6e36a555fe6de15fbdb9c9fb5ebee53ed59b1fa
Yanki <yankisirtas@hotmail.com>
2022-04-29 21:38:52 +0300

    Improve blog page featured image responsiveness

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -30,7 +30,7 @@ const Blog = ({ posts }: Props) => {
 
   return (
     <div className="bg-gray-200 mb-96">
-      <Image
+      {/* <Image
         className=""
         src={
           highlightedPost.coverImage.url ||
@@ -40,43 +40,52 @@ const Blog = ({ posts }: Props) => {
         layout="responsive"
         width={720}
         height={180}
-      />
+      /> */}
+      <div
+        className="w-full h-[60vh]"
+        style={{
+          backgroundImage: `url("/placeholder.jpg")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "fit",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="flex flex-row w-[90vw] pb-48 mx-auto space-x-6 overflow-x-scroll md:w-5/6 md:overflow-x-auto">
+          <div className="p-4 shadow-xl md:translate-y-0 translate-y-1/3 md:overflow-y-hidden md:p-10 md:w-2/3 rounded-xl bg-primary-500 md:mt-[32vh] md:h-[50vh]">
+            <div className="flex flex-col text-left">
+              <div className="text-sm font-light text-gray-300 uppercase md:text-base">
+                {highlightedPost.tags.join(", ")}
+              </div>
+              <h1 className="w-[60vw] md:w-full mt-2 text-xl font-bold text-white md:mt-10 md:text-4xl">
+                {highlightedPost.title}
+              </h1>
+              <p className="mt-6 text-sm text-gray-300">
+                {highlightedPost.excerpt}
+              </p>
 
-      <div className="flex flex-row w-5/6 mx-auto space-x-6">
-        <div className="w-2/3 p-10 -translate-y-1/2 shadow-xl rounded-xl bg-primary-500">
-          <div className="flex flex-col text-left">
-            <div className="font-light text-gray-300 uppercase">
-              {highlightedPost.tags.join(", ")}
-            </div>
-            <h1 className="mt-10 text-4xl font-bold text-white">
-              {highlightedPost.title}
-            </h1>
-            <p className="mt-6 text-sm text-gray-300">
-              {highlightedPost.excerpt}
-            </p>
-
-            <div className="p-3 mt-16 font-bold text-center text-black bg-white w-36 rounded-3xl">
-              Read More
-            </div>
-          </div>
-        </div>
-        {posts.slice(0, 2).map((post) => (
-          <div
-            key={post.id}
-            className="w-1/3 p-10 mt-6 bg-gray-700 shadow-xl h-1/3 rounded-xl"
-          >
-            <div className="flex flex-col space-y-5 text-left">
-              <h1 className="text-xl font-bold text-white">{post.title}</h1>
-              <p className="text-sm text-gray-300">{post.excerpt}</p>
-
-              <div className="p-3 font-bold text-center text-black bg-white w-36 rounded-3xl">
+              <div className="p-3 mt-8 font-bold text-center text-black bg-white md:mt-16 w-36 rounded-3xl">
                 Read More
               </div>
             </div>
           </div>
-        ))}
+          {posts.slice(0, 2).map((post) => (
+            <div
+              key={post.id}
+              className="w-[60vw] md:w-1/3 p-10 bg-gray-700 shadow-xl h-1/3 rounded-xl mt-auto translate-y-1/2 md:translate-y-0"
+            >
+              <div className="flex flex-col space-y-5 text-left">
+                <h1 className="text-xl font-bold text-white">{post.title}</h1>
+                <p className="text-sm text-gray-300">{post.excerpt}</p>
+
+                <div className="p-3 font-bold text-center text-black bg-white w-36 rounded-3xl">
+                  Read More
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="mx-auto">
+      <div className="mx-auto mt-80">
         <Tab.Group>
           <Tab.List className="flex flex-row p-1 mx-auto mb-6 overflow-scroll bg-white shadow-md md:overflow-auto md:justify-between md:max-w-5xl md:space-x-1 md:rounded-3xl">
             {uniqueTags.map((tag) => (
