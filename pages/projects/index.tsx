f245158bcbacc980751d11373c9eb18600c99b93
Yanki <yankisirtas@hotmail.com>
2022-05-01 19:46:14 +0300

    Improve projects page responsiveness

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -28,15 +28,17 @@ const Blog = ({ projects }: Props) => {
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
-            className="w-1/4 rounded-md shadow-lg h-[60vh] m-8 flex flex-col group"
+            className="w-1/3 md:w-1/4 rounded-md h-[60vh] m-4 md:m-8 flex flex-col group"
           >
-            <div className="hidden group-hover:flex flex-col items-center h-[40vh] bg-white mt-auto rounded-t-md px-1 md:px-8 justify-between bg-opacity-90 backdrop-filter backdrop-blur-[50px]">
-              <h1 className="mt-8 text-lg font-bold md:text-2xl">
+            <div className="md:hidden group-hover:flex flex-col items-center h-[20vh] md:h-[40vh] bg-white mt-auto rounded-t-md px-2 md:px-8 justify-between bg-opacity-90 backdrop-filter backdrop-blur-[50px]">
+              <h1 className="mb-8 text-sm font-bold md:mb-0 md:mt-8 md:text-2xl">
                 {project.name}
               </h1>
-              <div className="">{project.description}</div>
+              <div className="hidden text-xs md:block md:text-base text-ellipsis">
+                {project.description}
+              </div>
               <Link href={project.link as string}>
-                <a className="w-1/3 p-3 mb-4 mr-auto text-center text-white rounded-full bg-primary-500">
+                <a className="p-1 text-xs text-center text-white rounded-full md:mr-auto md:p-3 md:mb-4 md:w-1/3 bg-primary-500 md:text-base">
                   Read more{" "}
                 </a>
               </Link>
