8183d0e26b71240b550ed2ec4ed3c5bec8793f8c
Yankı Sırtaş <yankisirtas@hotmail.com>
2022-05-11 20:29:46 +0300

    Add <a> tag around logo image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,17 +25,20 @@ const Navbar = ({ bgColor, textColor, logo }: Props) => {
           <rect y="60" width="100" height="10"></rect>
         </svg>
         <Link href="/" passHref>
-          <BlurImage
-            className=""
-            src={
-              logo === "alt"
-                ? `${BASE_URL}/logo-alt.svg`
-                : `${BASE_URL}/logo.svg`
-            }
-            alt="3H Logo"
-            width={150}
-            height={150}
-          />
+          <a href="#">
+            {" "}
+            <BlurImage
+              className=""
+              src={
+                logo === "alt"
+                  ? `${BASE_URL}/logo-alt.svg`
+                  : `${BASE_URL}/logo.svg`
+              }
+              alt="3H Logo"
+              width={150}
+              height={150}
+            />
+          </a>
         </Link>
         <ul className="items-center hidden h-12 mx-auto space-x-10 font-bold text-primary-50 md:flex">
           <li>
