6cf597b7b8a753d15d6308d6c390def25e1e4d66
Ant Somers <antsomers@gmail.com>
2022-05-16 17:22:18 +0300

    Revert "Add lang attribute to Html"
    This reverts commit 1d15a73a719ddebdbdc4ffe5c2118a1c5e08fb57.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { useRouter } from "next/router";
 
 export default function Document() {
-  const router = useRouter();
-
   return (
-    <Html lang={router.locale === "tr" ? "tr" : "en"}>
+    <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
