1d15a73a719ddebdbdc4ffe5c2118a1c5e08fb57
Ant Somers <antsomers@gmail.com>
2022-05-16 17:19:53 +0300

    Add lang attribute to Html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useRouter } from "next/router";
 
 export default function Document() {
+  const router = useRouter();
+
   return (
-    <Html>
+    <Html lang={router.locale === "tr" ? "tr" : "en"}>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
