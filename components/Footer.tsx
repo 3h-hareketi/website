3e80f3d9152ab430bf5360e43adc3ca3dec9cfa4
Ant Somers <antsomers@gmail.com>
2022-05-17 17:54:40 +0300

    Add some translation to Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,13 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import BlurImage from "./BlurImage";
 import logo from "/public/3h-vector.png";
 
 const Footer = () => {
+  const t = useTranslations("Footer");
+
   return (
-    <section className="bg-primary-500">
+    <footer className="bg-primary-500">
       <div className="flex flex-col items-center mt-16 md:flex-row md:mx-64">
         <div className="space-y-3 text-center md:mr-auto">
           <BlurImage src={logo} height={128} width={128} className="" />
@@ -16,7 +19,7 @@ const Footer = () => {
           </ul>
         </div>
         <h1 className="text-4xl font-bold text-center text-white md:text-6xl">
-          Reach out to us!
+          {t("motto")}
         </h1>
 
         <div className="flex flex-col mt-5 ml-10 mr-auto text-left md:mr-0 md:mt-0 md:ml-auto md:text-right">
@@ -25,44 +28,44 @@ const Footer = () => {
             <li>LEARN MORE</li>
             <Link href="/about" passHref>
               <li className="font-thin">
-                <a href="#">About Us</a>
+                <a>About Us</a>
               </li>
             </Link>
             <Link href="/contact" passHref>
               <li className="font-thin">
-                <a href="#">Contact Us</a>
+                <a>Contact Us</a>
               </li>
             </Link>
             <Link href="/projects" passHref>
               <li className="font-thin">
-                <a href="#">Projects</a>
+                <a>Projects</a>
               </li>
             </Link>{" "}
             <li className="mt-16">SUPPORT</li>
             <Link href="/terms" passHref>
               <li className="font-thin">
-                <a href="#">Terms and Services</a>
+                <a>Terms and Services</a>
               </li>
             </Link>{" "}
             <Link href="/privacy" passHref>
               <li className="font-thin">
-                <a href="#">Privacy</a>
+                <a>Privacy</a>
               </li>
             </Link>
             <Link href="/cookies" passHref>
               <li className="font-thin">
-                <a href="#"> Cookies </a>
+                <a> Cookies </a>
               </li>
             </Link>
           </ul>
         </div>
       </div>
-      <div className="flex text-sm text-gray-300 font-extralight">
+      <div className="flex mb-3 text-sm text-gray-300 font-extralight">
         <div className="mx-auto">
-          {"© " + new Date().getFullYear() + " All rights reserved."}
-        </div>{" "}
+          {"© " + new Date().getFullYear() + ` ${t("copyright")}`}
+        </div>
       </div>
-    </section>
+    </footer>
   );
 };
 
