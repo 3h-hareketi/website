3d48d7dfcdc4db33f17fc2a491b7652501b8f44c
Ant Somers <antsomers@gmail.com>
2022-05-20 03:00:32 +0300

    Install and integrate react-google-recaptcha-v3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.GOOGLE_RECAPTCHA_KEY}
+      useEnterprise={true}
+    >
+      <Component {...pageProps} />
+    </GoogleReCaptchaProvider>
+  );
 }
 
 export default MyApp;
