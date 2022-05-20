5af80e7aa26c49e7a781e10aa550f6f203006d5d
Ant Somers <antsomers@gmail.com>
2022-05-20 03:11:15 +0300

    Add language support to GoogleReCaptchaProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={process.env.GOOGLE_RECAPTCHA_KEY}
       useEnterprise={true}
+      language={router.locale}
     >
       <Component {...pageProps} />
     </GoogleReCaptchaProvider>
