a3ec3228f2cd69d34d3b14f6c30ed365f5f1cf72
Ant Somers <antsomers@gmail.com>
2022-06-01 23:17:22 +0300

    Install & integrate react-gtm-hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextIntlProvider } from "next-intl";
+import { GTMProvider } from "@elgorditosalsero/react-gtm-hook";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextIntlProvider messages={pageProps.messages} timeZone="Europe/Istanbul">
-      <Component {...pageProps} />
-    </NextIntlProvider>
+    <GTMProvider state={{ id: process.env.NEXT_PUBLIC_GTM! }}>
+      <NextIntlProvider
+        messages={pageProps.messages}
+        timeZone="Europe/Istanbul"
+      >
+        <Component {...pageProps} />
+      </NextIntlProvider>
+    </GTMProvider>
   );
 }
 
