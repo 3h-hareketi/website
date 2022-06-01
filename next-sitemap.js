2bca0a6957b51c6e040b673d09b70f2cb7e1a0e3
Ant Somers <antsomers@gmail.com>
2022-06-01 22:55:55 +0300

    Fix next-sitemap.siteUrl

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: "https" + process.env.VERCEL_URL || "https://www.3hhareketi.org",
+  siteUrl: "https://" + process.env.VERCEL_URL || "https://www.3hhareketi.org",
   generateRobotsTxt: true,
 };
