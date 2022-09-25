117eb4b59d67cf6f4259acb88cf74739b413e4ee
Ant Somers <antsomers@gmail.com>
2022-09-25 21:40:57 +0300

    fix: baseUrl for production

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production" || process.env.VERCEL_ENV === "preview"
-    ? "https://" + process.env.VERCEL_URL
+    ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
     : "http://localhost:3000";
 
 export default BASE_URL;
