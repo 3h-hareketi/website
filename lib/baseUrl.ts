1926a716d08013092a89f4e33be4f82bb8ea922c
Ant Somers <antsomers@gmail.com>
2022-09-25 21:45:11 +0300

    chore: add NEXT_PUBLIC_SITE_URL support to baseUrl

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production" || process.env.VERCEL_ENV === "preview"
-    ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
+    ? "https://" + process.env.NEXT_PUBLIC_SITE_URL
     : "http://localhost:3000";
 
 export default BASE_URL;
